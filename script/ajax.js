dc48e2219a7f7bf3031f903951e240eb346059a2
YDECORCE <y.decorce@codeur.online>
2020-12-08 09:04:34 +0100

    recherche bug

--- script/ajax.js
+++ script/ajax.js
@@ -1,12 +1,14 @@
 // Exécute un appel AJAX GET
-// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
-function ajaxGet(url, callback) {
+// Prend en paramètres l'URL cible
+function ajaxGet(url) {
+    var data;
     var req = new XMLHttpRequest();
-    req.open("GET", url);
+    req.open("GET", url, true);
     req.addEventListener("load", function () {
         if (req.status >= 200 && req.status < 400) {
-            // Appelle la fonction callback en lui passant la réponse de la requête
-            callback(req.responseText);
+            data=JSON.parse(req.responseText);
+            console.log(data);
+            
         } else {
             console.error(req.status + " " + req.statusText + " " + url);
         }
@@ -15,4 +17,5 @@ function ajaxGet(url, callback) {
         console.error("Erreur réseau avec l'URL " + url);
     });
     req.send(null);
+    return data;
 }
--- script/movies.js
+++ script/movies.js
@@ -1,17 +1,19 @@
 var preview=document.getElementById("allmovies");
-var bbdFilms=[];
-var film=[];
+var AllMovies=[];
+
+// var film=[];
 
 function Movie(index) {
     this.preview=function(){
+        // console.log(bbdFilms)
         var movie=document.createElement("div");
         movie.id='movie'+index;
-        film=bbdFilms[index];
-        console.log(film);
+        // film=bbdFilms[index];
+        // console.log(film);
         movie.classList="col-12 col-sm-6 col-lg-3";
         var inMovie=document.createElement("div");
         inMovie.classList="movie my-3 mx-1";
-        inMovie.innerHTML=`<h2>${film[title]}</h2>`;
+        // inMovie.innerHTML=`<h2>${bbdFilms[index].title}</h2>`;
         preview.appendChild(movie);
         movie.appendChild(inMovie);
         }
@@ -20,14 +22,12 @@ function Movie(index) {
 
 
 function generate(){
-
-ajaxGet("https://api.themoviedb.org/3/movie/upcoming?api_key=d5547c6cdbe3cbaed33b74459d673b62&language=fr-fr&page=1&region=FR",
-    function (reponse) {
-        var allmovies = JSON.parse(reponse);
-        console.log(allmovies);
-        bbdFilms=allmovies.results;
+var bbdFilms=[];
+bbdFilms=ajaxGet("https://api.themoviedb.org/3/movie/upcoming?api_key=d5547c6cdbe3cbaed33b74459d673b62&language=fr-fr&page=1&region=FR");
+   
+        // AllMovies=bbdFilms.results[];
         console.log(bbdFilms);
-        })
+
 
 var films=[];
 for( var i=0; i<20; i++){
