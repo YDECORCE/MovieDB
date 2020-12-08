e6b823cdf0a1dbe546357abd5fddb14272f3b24c
YDECORCE <y.decorce@codeur.online>
2020-12-08 10:35:28 +0100

    avant retour branch main

--- script/movies.js
+++ script/movies.js
@@ -7,37 +7,27 @@ generate();
 
 function generate(){
     
-    console.log(toto);
     ajaxGet("https://api.themoviedb.org/3/movie/upcoming?api_key=d5547c6cdbe3cbaed33b74459d673b62&language=fr-fr&page=1&region=FR", function(reponse)
             {
         var toto=JSON.parse(reponse);
-        console.log(toto);
-        return toto;
-            });
-       console.log (toto);
         AllMovies=toto['results'];
         console.log(AllMovies);
-            // console.log(bbdFilms);
-       
-   
-    for( var i=0; i<20; i++)
+        for( var i=0; i<20; i++)
         {
-        console.log(i);
         films[i]= new Movie(i);
         films[i].preview();
         }
-    return films;
-    }
+        return films;
+     
+            });
+}
 
 
 function ajaxGet(url, callback) {
-    // var data;
     var req = new XMLHttpRequest();
     req.open("GET", url, true);
     req.addEventListener("load", function () {
         if (req.status >= 200 && req.status < 400) {
-            // data=JSON.parse(req.responseText);
-            // console.log(req.responseText);
             callback(req.responseText)
             
         } else {
@@ -48,20 +38,20 @@ function ajaxGet(url, callback) {
         console.error("Erreur réseau avec l'URL " + url);
     });
     req.send();
-    // return data;
+   
 }
 
 function Movie(index) {
+    this.title = AllMovies[index].title
+    this.urlaffiche=AllMovies[index].poster_path;
     this.preview=function(){
-        // console.log(bbdFilms)
+        // console.log(AllMovies)
         var movie=document.createElement("div");
         movie.id='movie'+index;
-        // film=bbdFilms[index];
-        // console.log(film);
         movie.classList="col-12 col-sm-6 col-lg-3";
         var inMovie=document.createElement("div");
         inMovie.classList="movie my-3 mx-1";
-        // inMovie.innerHTML=`<h2>${bbdFilms[index].title}</h2>`;
+        inMovie.innerHTML=`<h2>${this.title}</h2>`;
         preview.appendChild(movie);
         movie.appendChild(inMovie);
         }
