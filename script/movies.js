a4785eb69cfe980c7ce1acd5bd83cec59fc8691c
YDECORCE <y.decorce@codeur.online>
2020-12-08 17:05:57 +0100

    carousel Netflix like

--- script/movies.js
+++ script/movies.js
@@ -1,28 +1,22 @@
+var Racine = document.getElementById("racine");
 var preview=document.getElementById("allmovies");
 var AllMovies=[];
-var toto=[];
+var Upcoming=[];
 var films=[];
 generate();
 
 
 function generate(){
     
-    console.log(toto);
-    ajaxGet("https://api.themoviedb.org/3/movie/upcoming?api_key=d5547c6cdbe3cbaed33b74459d673b62&language=fr-fr&page=1&region=FR", function(reponse)
-            {
-        var toto=JSON.parse(reponse);
-        console.log(toto);
-        return toto;
-            });
-       console.log (toto);
-        AllMovies=toto['results'];
-        console.log(AllMovies);
-            // console.log(bbdFilms);
-       
-   
-    for( var i=0; i<20; i++)
+    
+    Upcoming=ajaxGet("https://api.themoviedb.org/3/movie/upcoming?api_key=d5547c6cdbe3cbaed33b74459d673b62&language=fr-fr&page=1&region=FR");
+    AllMovies=Upcoming['results'];
+    console.log(AllMovies);
+    modal = new Modal();
+    modal.content(); 
+       for( var i=0; i<20; i++)
         {
-        console.log(i);
+        // console.log(i);
         films[i]= new Movie(i);
         films[i].preview();
         }
@@ -30,44 +24,97 @@ function generate(){
     }
 
 
-function ajaxGet(url, callback) {
-    // var data;
-    var req = new XMLHttpRequest();
-    req.open("GET", url, true);
-    req.addEventListener("load", function () {
-        if (req.status >= 200 && req.status < 400) {
-            // data=JSON.parse(req.responseText);
-            // console.log(req.responseText);
-            callback(req.responseText)
-            
-        } else {
-            console.error(req.status + " " + req.statusText + " " + url);
-        }
-    });
-    req.addEventListener("error", function () {
-        console.error("Erreur réseau avec l'URL " + url);
-    });
-    req.send();
-    // return data;
-}
 
 function Movie(index) {
+    this.title=AllMovies[index].title;
+    this.urlposter="'https://image.tmdb.org/t/p/w500"+AllMovies[index].poster_path+"'";
+    this.Idmovie=AllMovies[index].id;
+    this.overview=AllMovies[index].overview;
+    // console.log(this.urlposter)
     this.preview=function(){
-        // console.log(bbdFilms)
+        
         var movie=document.createElement("div");
         movie.id='movie'+index;
-        // film=bbdFilms[index];
-        // console.log(film);
-        movie.classList="col-12 col-sm-6 col-lg-3";
+       
+        movie.classList="col-12 col-lg-4 carousel-tile";
         var inMovie=document.createElement("div");
-        inMovie.classList="movie my-3 mx-1";
-        // inMovie.innerHTML=`<h2>${bbdFilms[index].title}</h2>`;
+        inMovie.classList="movie justify-content-center my-3 mx-1";
+        // inMovie.innerHTML=`<h2>${this.title}</h2>`;
+        // inMovie.innerHTML='<img src='+this.urlposter+' alt="affichefilm" style="width:auto; max-height:350px">'
+        inMovie.style.backgroundImage='url('+this.urlposter+')';
+        inMovie.style.backgroundSize="100% auto"
         preview.appendChild(movie);
         movie.appendChild(inMovie);
-        }
+        // génération contenu Modal
+        movie.onclick = makeOnClickCallback(this.title,this.overview)
+        // movie.onclick=function(){
+        //     var modal = document.getElementById("exampleModalLive");
+        //     var span = document.getElementsByClassName("close")[0];
+        //     var modalContent = document.getElementById("modalContent");
+        //     var titre = document.getElementById("titre");
+        //     modal.classList.toggle("show");
+        //     modal.style.zIndex = "1";
+        //     modalContent.textContent = 'WTF';
+        //     titre.innerHTML = '<h2>BIG WTF</h2>';
+        
+        //     span.onclick = function () {
+        //         modal.classList.toggle("show");
+        //         modal.style.zIndex = "-1";}    
+        // }
+}
 }
 
+//Génération de la Modal
+function Modal() {
+    this.content = function () {
+        //Génération de la modal
+        var modal = document.createElement("div");
+        Racine.append(modal);
+        modal.id = "exampleModalLive";
+        modal.classList = "modal faded";
+        modal.style.display = "block";
+        modal.style.zIndex = "-1";
+        //Génération de la modal content
+        var modalContent = document.createElement("div");
+        modal.append(modalContent);
+        modalContent.classList = "modal-content";
+        //Génération du bouton close
+        var close = document.createElement("span");
+        modalContent.append(close);
+        close.classList = "close";
+        close.innerHTML = "&times;";
+        //Génération du titre de la Modal
+        var h1 = document.createElement("h2");
+        modalContent.append(h1);
+        h1.id = "titre";
+        //Génération du paragraphe qui accueillera le cadeau
+        var para = document.createElement("p");
+        modalContent.append(para);
+        para.id = "modalContent";
+    }
+}
 
-
-
+function makeOnClickCallback(title,overview) 
+{
+    return function() 
+    {
+        console.log(title)
+        console.log(overview)       
+        var modal = document.getElementById("exampleModalLive");
+        var span = document.getElementsByClassName("close")[0];
+        var modalContent = document.getElementById("modalContent");
+        var titre = document.getElementById("titre");
+        modal.classList.toggle("show");
+        modal.style.zIndex = "1";
+        modalContent.innerHTML = '<p>'+overview+'</p>';
+        titre.innerHTML = '<h2>'+title+'</h2>'
+    
+        span.onclick = function () 
+        {
+            modal.classList.toggle("show");
+            modal.style.zIndex = "-1";
+        }   
+        return false;
+    }
+}
 
